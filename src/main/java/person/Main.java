53bab5b241bcca84583018cdaaf23ff841981e04
iGyalchester <borisgerard333@gmail.com>
2021-09-29 12:41:49 -0400

    Finished testing person

---
+++ src/main/java/person/Main.java
@@ -0,0 +1,13 @@
+package person;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person person = new Person("Hanna", 25);
+		person.setName("Robert");
+		person.setAge(26);
+
+		System.out.println(person);
+
+	}
+}
