c8df219e5e6e7e6f9ab8ecdef7329f354810abbf
iGyalchester <borisgerard333@gmail.com>
2021-09-29 11:33:00 -0400

    "Forgot to uncomment"

--- src/main/java/Grade.java
+++ src/main/java/Grade.java
@@ -2,18 +2,17 @@ public class Grade {
 
 	public String printGrade(int grade) {
 
-//
-//		if(grade >= 90){
-//			return "A";
-//		} else if(grade >=80){
-//			return "B";
-//		} else if(grade >=70) {
-//			return "C";
-//		} else if(grade >=60){
-//			return "D";
-//		}else{
-//			return "F";
-//		}
-		return null;
+
+		if(grade >= 90){
+			return "A";
+		} else if(grade >=80){
+			return "B";
+		} else if(grade >=70) {
+			return "C";
+		} else if(grade >=60){
+			return "D";
+		}else{
+			return "F";
+		}
 	}
 }
